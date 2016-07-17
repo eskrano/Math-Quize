f9ea313a5965e379b77f4e3b473d68aa773f8015
eskrano <alex.priadko2014@yandex.ru>
2016-07-18 02:52:21 +0300

    New features

--- scripts/app.js
+++ scripts/app.js
@@ -6,6 +6,9 @@ window.onload = (function() {
   var bar = document.getElementsByClassName('progress-bar')[0];
   var barProgress = document.getElementsByClassName('progress-bar-condition')[0];
   barProgress.style.width = 0;
+  var score = 0;
+
+      var count = 0; //progressbar count
 
 
   function Quize() {
@@ -42,7 +45,7 @@ window.onload = (function() {
     progress ();
     var a = getRandomInt(1,20);
     var b = getRandomInt(1,20);
-    var result = a + b;
+    var result = (a + b ) +  getRandomInt(0,5);
     var aValue = document.querySelector(".a-value");
     aValue.textContent = a;
 
@@ -63,18 +66,90 @@ window.onload = (function() {
 
 
   function progress () {
-    yes.addEventListener("click",updateProgressBar);
+    yes.addEventListener("click",yesClickEvent);
+    quize.noButton.addEventListener("click",noClickEvent);
   }
 
+
+function noClickEvent()
+{
+  updateProgressBarCount();
+
+  checkIsRightAnsver(false);
+}
+
+
+function updateScore()
+{
+  score++;
+  document.getElementById('score').textContent = score;
+}
+
+function yesClickEvent()
+{
+  updateProgressBarCount();
+  checkIsRightAnsver(true);
+}
+
+function updateProgressBarCount()
+{
+  if (count > 0)
+  {
+      count =  count - (count / 2);
+  }
+
+}
+
+function checkIsRightAnsver(is_yes)
+{
+  var a = document.querySelector('.a-value').textContent;
+  var b = document.querySelector('.b-value').textContent;
+  var an = document.querySelector('.answer').textContent;
+
+
+  a = parseInt(a); 
+  b = parseInt(b);
+  an = parseInt(an);
+
+  if (is_yes) {
+    if (a + b ==  an) {
+      updateScore();
+      return getNextTask();
+    }
+    return triggerEndGame();
+  } else {
+    if (a + b !=  an) {
+      updateScore();
+      return getNextTask();
+    }
+    return triggerEndGame();
+  }
+
+}
+
+
+function getNextTask()
+{
+  return getTask();
+}
+
+function triggerEndGame()
+{
+  document.querySelector('.quize').style.display = 'none';
+  document.querySelector('.gameover').style.display = 'block';
+  score = 0;
+}
  
  function updateProgressBar() {
-      var count = 0;
+
       var interval = setInterval(function() {
-        if (count >= 100) {
+        if (count >= 127) {
+
           clearInterval(interval);
-          return;
+          return triggerEndGame();
         }
         count++;
+        console.log(count);
         barProgress.style.width = count + '%';
       }, 50);
 
